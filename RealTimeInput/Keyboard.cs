dec68458cd7f33db9021e0f3228f94da29701d24
CeniusM <93879193+CeniusM@users.noreply.github.com>
2024-11-09 23:50:44 +0100

    Add project files.

---
+++ RealTimeInput/Keyboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RealTimeInput;
+
+public class Keyboard
+{
+    [DllImport("user32.dll")]
+    private static extern short GetKeyState(int nVKey);
+
+    public static bool IsKeyDown(int key)
+    {
+        return (GetKeyState(key) & (0b1000_0000_0000_0000)) != 0;
+    }
+
+    public static bool IsKeyToggled(int key)
+    {
+        return (GetKeyState(key) & (0b1)) != 0;
+    }
+}
---
+++ RealTimeInput/Program.cs
@@ -0,0 +1,25 @@
+﻿using RealTimeInput;
+
+Console.CursorVisible = false;
+
+while (true)
+{
+    int lines = 0;
+    for (int i = 0; i < 256; i++)
+    {
+        if (Keyboard.IsKeyDown(i))
+        {
+            Console.WriteLine(i + " down");
+            lines++;
+        }
+    }
+
+    Thread.Sleep(100);
+
+    Console.SetCursorPosition(0, 0);
+
+    for (int i = 0; i < lines; i++)
+        Console.WriteLine(new string(' ', 8));
+
+    Console.SetCursorPosition(0, 0);
+}
